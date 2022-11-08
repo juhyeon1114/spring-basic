84acf16d7290f1a77526742dd724ead53f49f2dc
Kai Kim <juhyeon1114@gmail.com>
2022-11-08 22:47:34 +0900

    log: 주문과 할인 도메인 개발

---
+++ src/main/java/hello/springbasic/discount/FixDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.springbasic.discount;
+
+import hello.springbasic.member.Grade;
+import hello.springbasic.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        }
+        return 0;
+    }
+}

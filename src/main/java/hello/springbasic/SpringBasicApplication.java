29735ae7f0d2d90550cb9e03fa471d6fa9052fcc
Kai Kim <juhyeon1114@gmail.com>
2022-11-08 21:46:57 +0900

    update: 기본 기능

--- src/main/java/hello/springbasic/SpringBasicApplication.java
+++ src/main/java/hello/springbasic/SpringBasicApplication.java
@@ -1,13 +1,27 @@
 package hello.springbasic;
 
+import hello.springbasic.member.Grade;
+import hello.springbasic.member.Member;
+import hello.springbasic.member.MemberService;
+import hello.springbasic.member.MemberServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SpringBasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBasicApplication.class, args);
+//		SpringApplication.run(SpringBasicApplication.class, args);
+		MemberService memberService = new MemberServiceImpl();
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		memberService.join(member);
+
+		Member foundMember = memberService.findMember(1L);
+		System.out.println("new member = " + member.getName());
+		System.out.println("find Member = " + foundMember.getName());
+
 	}
 
 }

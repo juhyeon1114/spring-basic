e6f2c14f4a735667ec5ef99003b1960cf3e036fb
Kai Kim <juhyeon1114@gmail.com>
2022-11-09 22:58:16 +0900

    log : 스프링으로 전환하기

--- src/main/java/hello/springbasic/MemberApp.java
+++ src/main/java/hello/springbasic/MemberApp.java
@@ -3,11 +3,24 @@ package hello.springbasic;
 import hello.springbasic.member.Grade;
 import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        /**
+         * 스프링 컨테이너 적용 전
+         */
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /**
+         * 스프링 컨테이너 적용 후
+         */
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
--- src/main/java/hello/springbasic/OrderApp.java
+++ src/main/java/hello/springbasic/OrderApp.java
@@ -5,12 +5,25 @@ import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberService;
 import hello.springbasic.order.Order;
 import hello.springbasic.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        /**
+         * 스프링 컨테이너 적용 전
+         */
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        /**
+         * 스프링 컨테이너 적용 후
+         */
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);

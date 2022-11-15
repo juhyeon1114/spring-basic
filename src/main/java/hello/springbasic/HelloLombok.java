05b3a0df29ffc00390596671974606eee4eab032
Kai Kim <juhyeon1114@gmail.com>
2022-11-15 11:06:37 +0900

    log : Lombok

---
+++ src/main/java/hello/springbasic/HelloLombok.java
@@ -0,0 +1,21 @@
+package hello.springbasic;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter // getter를 직접 선언하지 않아도 사용할 수 있다.
+@Setter // setter를 직접 선언하지 않아도 사용할 수 있다.
+@ToString // toString을 직접 선언하지 않아도, 출력할 때 toString()을 적용해준다.
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("wow");
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
--- src/main/java/hello/springbasic/order/OrderServiceImpl.java
+++ src/main/java/hello/springbasic/order/OrderServiceImpl.java
@@ -3,18 +3,22 @@ package hello.springbasic.order;
 import hello.springbasic.discount.DiscountPolicy;
 import hello.springbasic.member.Member;
 import hello.springbasic.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final 또는 @NotNull이 붙은 필드에 대한 constructor를 자동으로 생성해줌. -> constructor를 따로 만들필요도 없고, @Autowired를 쓸 필요도 없다.
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
